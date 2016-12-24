3c395718742f698ec5951fee4a75873ac24f0531
Jam Risser <jamrizzi@gmail.com>
2016-12-24 02:31:20 -0600

    Small changes

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-cp /home/jamrizzi/Projects/devos-user/tmux/.tmux.conf ~/.tmux.conf
+
+# autostart
 tee -a ~/.zshrc 2>/dev/null <<EOF
 if [[ -z "\$TMUX" ]]; then
         tmux
 fi
 EOF
+
+# plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# configuration
+wget -P ~ https://raw.githubusercontent.com/jamrizzi/staz-ide/master/tmux/.tmux.conf
 tmux source-file ~/.tmux.conf
+
+git clone https://github.com/erikw/tmux-powerline.git ~/.tmux/
+cp ~/.tmux/tmux-powerline/themes/default.sh ~/.tmux-theme.sh
+$EDITOR ~/.tmux-theme.sh
+~/.tmux/tmux-powerline/generate_rc.sh
+mv ~/.tmux-powerlinerc.default ~/.tmux-powerlinerc
+$EDITOR ~/.tmux-powerlinerc
