65fbb221452cdedfebe719e9e147bcba8fd9f95d
Jam Risser <jam@jamrizzi.com>
2017-03-03 02:20:39 -0600

    Install dependancies

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python2.7
+pip install future
 git clone https://github.com/jamrizzi/staz-ide.git
 python ./staz-ide/src/install.py
 rm -r ./staz-ide
